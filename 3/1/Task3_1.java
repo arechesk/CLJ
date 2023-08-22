8e3c4cb325548449c817fc65ac21182afa7caa0c
Aleksandr Kulikov <unnotigkeit@yandex.ru>
2023-08-22 13:35:20 +0300

    Update Task3_1.java

--- 3/1/Task3_1.java
+++ 3/1/Task3_1.java
@@ -1,1 +1,10 @@
+import java.util.*;
+public class Task3_1 {
+    public static void main(String[] args){
+        List<Integer> sll= new LinkedList<>(List.of(1,2,3,4,5,6,7,8,9));
+        System.out.println(sll);
+        Collections.reverse(sll);
+        System.out.println(sll);
+    }
+}
 
